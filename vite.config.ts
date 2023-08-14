eac7b0a44904cd243b4ce09cf2da519a09b6bafa
Joshua Pease <joshua.pease@viget.com>
2023-08-14 11:22:57 -0700

    Package up CSS into JS file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
 
 export const sharedConfig = {
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    cssInjectedByJsPlugin(),
+  ],
 }
 
 // https://vitejs.dev/config/
