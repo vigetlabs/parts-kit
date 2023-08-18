47ee7579bd9ac735dc546fb851e6a64c9bffea47
Joshua Pease <joshua.pease@viget.com>
2023-08-17 17:35:42 -0700

    Add vitest and VSCode debug launcher

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig, mergeConfig, UserConfig } from 'vite'
+import { mergeConfig, UserConfig } from 'vite'
+import { defineConfig} from 'vitest/config'
 import preact from '@preact/preset-vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 
 /** Config shared by all of our build processes. Used with `mergeConfig` */
-export const sharedConfig: UserConfig = {
+export const sharedConfig: UserConfig = defineConfig({
   plugins: [
     preact(),
     cssInjectedByJsPlugin(),
   ],
-}
+  test: {
+    globals: true,
+  }
+})
 
 export default mergeConfig(sharedConfig, {
   // Customize your local dev here
