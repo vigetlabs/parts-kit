001b5a6664b98914ea0ae8410350717f480f2280
Joshua Pease <joshua.pease@viget.com>
2023-09-20 10:24:06 -0700

    POC Arbitrary Screen Size. Kinda broken. Kinda works

--- src/features/viewport/Viewport.tsx
+++ src/features/viewport/Viewport.tsx
@@ -1,3 +1,4 @@
+import { Resizable } from "re-resizable";
 import cx from "classnames";
 import { NavItemInterface } from "../nav/Nav";
 import { uniqueId } from "../../utilities/string";
@@ -6,31 +7,60 @@ import {
   screenSizeMap,
   useUtilityBarStore,
 } from "../utility-bar/store";
+import { useCallback } from "preact/hooks";
+import { memo } from "preact/compat";
 
 interface ViewportProps {
   activeNavItem: NavItemInterface | undefined;
 }
 
+interface FrameComponentProps {
+  url: string;
+}
+
+function FrameComponent(props: FrameComponentProps) {
+  // Changing the src of iframes will muck up your history. Using key to rerender when the nav changes is a workaround
+  return (
+    <iframe
+      // key={props.activeNavItem?.url + uniqueId()}
+      className={cx("w-full h-full", {
+        "border-2 rounded border-gray-100": true,
+        // utilityBar.activeScreenSize !== ScreenSize.Desktop,
+      })}
+      src={props.url ?? undefined}
+    ></iframe>
+  );
+}
+
+const MemoedFrameComponent = memo(FrameComponent);
+
 export default function (props: ViewportProps) {
-  const utilityBar = useUtilityBarStore();
-  const activeScreenWidth = screenSizeMap[utilityBar.activeScreenSize];
+  // const utilityBar = useUtilityBarStore();
+  // const activeScreenWidth = screenSizeMap[utilityBar.activeScreenSize];
 
   return (
-    <div
-      className={cx("flex-grow", {
-        "py-5": utilityBar.activeScreenSize !== ScreenSize.Desktop,
-      })}
-      style={{ maxWidth: activeScreenWidth }}
+    // Probably a TS error because of preact
+    <Resizable
+      enable={{
+        top: false,
+        right: true,
+        bottom: false,
+        left: true,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false,
+      }}
     >
-      {/* Changing the src of iframes will muck up your history. Using key to rerender when the nav changes is a workaround */}
-      <iframe
-        key={props.activeNavItem?.url + uniqueId()}
-        className={cx("w-full h-full", {
-          "border-2 rounded border-gray-100":
-            utilityBar.activeScreenSize !== ScreenSize.Desktop,
-        })}
-        src={props.activeNavItem?.url ?? undefined}
-      ></iframe>
-    </div>
+      <MemoedFrameComponent url={props.activeNavItem?.url ?? ''} />
+    </Resizable>
+
+    // <div
+    //   className={cx("flex-grow", {
+    //     "py-5": utilityBar.activeScreenSize !== ScreenSize.Desktop,
+    //   })}
+    //   style={{ maxWidth: activeScreenWidth }}
+    // >
+    // </div>
   );
 }
