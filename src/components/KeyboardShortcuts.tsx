b26f2a4d5deb9d9263a3860bea5d1a3eabfa6463
Joshua Pease <joshua.pease@viget.com>
2024-02-02 11:04:22 -0800

    [#17] Adds keyboard support when focus is in iframe. (#37)
    * [#17] Adds keyboard support when focus is in iframe.
    
    Uses postMessage to send pseudo keyboard events to our React app
    
    * [#17] PR Feedback

--- src/components/KeyboardShortcuts.tsx
+++ src/components/KeyboardShortcuts.tsx
@@ -1,51 +1,90 @@
 import { useEffect } from 'preact/hooks'
-import { useThemeStore, useUtilityBarStore } from '../features/utility-bar/store'
+import {
+  useThemeStore,
+  useUtilityBarStore,
+} from '../features/utility-bar/store'
+import { usePostMessageReceiver } from '../features/iframe-postmessage/receiver'
+import { isKeyboardEventMessage } from '../features/iframe-postmessage/messages'
+
+/**
+ * A simplified version of a keyboard event.
+ * preventDefault is optional so we can safely
+ * serialize it in a postMessage
+ */
+export interface SimpleKeyboardEvent {
+  key: string
+  shiftKey: boolean
+  preventDefault?(): void
+}
+
+export function canHandleKeyboard(): boolean {
+  return !(
+    document.activeElement instanceof HTMLInputElement ||
+    document.activeElement instanceof HTMLTextAreaElement
+  )
+}
+
+/**
+ * Safely preventDefault when we can
+ */
+function preventDefault(e: SimpleKeyboardEvent) {
+  if (e.preventDefault !== undefined) {
+    e.preventDefault()
+  }
+}
 
 export default function () {
   const utilityStore = useUtilityBarStore()
   const themeStore = useThemeStore()
 
+  usePostMessageReceiver({
+    onMessage(e) {
+      if (!isKeyboardEventMessage(e.data)) {
+        return
+      }
+
+      keydownHandler(e.data.payload)
+    },
+  })
+
   // handles different keyboard shortcuts
-  const keydownHandler = (e: KeyboardEvent) => {
-    // don't assess shortcuts when inside of an input or textarea
-    if (
-      document.activeElement instanceof HTMLInputElement ||
-      document.activeElement instanceof HTMLTextAreaElement
-    ) {
+  const keydownHandler = (e: SimpleKeyboardEvent) => {
+    if (!canHandleKeyboard()) {
       return
     }
 
-    switch (e.key) {
+    // Make sure all of your case statements are lowercase too
+    switch (e.key.toLowerCase()) {
       // toggle fullscreen [F]
       case 'f':
-        e.preventDefault()
+        preventDefault(e)
         return utilityStore.setIsNavBarVisible(!utilityStore.isNavBarVisible)
 
       // exit fullscreen [Esc]
-      case 'Escape':
-        e.preventDefault()
+      case 'escape':
+        preventDefault(e)
         return utilityStore.setIsNavBarVisible(true)
 
       // toggle viewport menu [Shift + V]
-      case 'V':
+      case 'v':
         if (e.shiftKey) {
-          e.preventDefault()
+          preventDefault(e)
           utilityStore.setIsViewportOpen(!utilityStore.isViewportOpen)
         }
         return
 
       // toggle theme [Shift + T]
-      case 'T':
+      case 't':
         if (e.shiftKey) {
-          e.preventDefault()
+          preventDefault(e)
           themeStore.toggleMode()
         }
         return
 
       // toggle settings [Shift + S]
-      case 'S':
+      case 's':
         if (e.shiftKey) {
-          e.preventDefault()
+          preventDefault(e)
           utilityStore.setIsSettingsOpen(!utilityStore.isSettingsOpen)
         }
         return
